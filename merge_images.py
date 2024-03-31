b8a76e5eaf2694980b380403f7ad70f3488fe5e5
ibean <imbean@qq.com>
2024-03-31 23:08:58 +0800

    Update merge_images.py
    adjust to mutiple image size

--- merge_images.py
+++ merge_images.py
@@ -17,27 +17,43 @@ class MergeImages:
     CATEGORY = "image/postprocessing"
 
     def merge(self, images):
-        batch, h, w, c = images.shape
+        # images.shape: batch, h, w, c
+        batch = len(images)
         if batch == 1:
             return images.unsqueeze(0)
+    
+        max_h = max(image.shape[0] for image in images)
+        max_w = max(image.shape[1] for image in images)
+        c = images[0].shape[2]
+    
         x = math.ceil(math.sqrt(batch))
         y = x - 1
         if x * y < batch:
             y = x
         total_images = x * y
     
-        # Create a black image for padding
-        black_img = torch.zeros((h, w, c))
+        # Create a white image for padding
+        white_img = torch.ones((max_h, max_w, c))
+        black_img = torch.zeros((max_h, max_w, c))
     
-        # Add black images to the list to make the total number of images a perfect square
-        for i in range(total_images - batch):
-            images = torch.cat((images, black_img.unsqueeze(0)), 0)
+        # Resize images and add white images to the list to make the total number of images a perfect square
+        resized_images = []
+        for i in range(total_images):
+            if i < batch:
+                img = images[i]
+                h, w = img.shape[0], img.shape[1]
+                if h < max_h or w < max_w:
+                    img = torch.cat((img, white_img[h:, :w]), 0)
+                    img = torch.cat((img, white_img[:h, w:]), 1)
+                resized_images.append(img)
+            else:
+                resized_images.append(black_img)
     
         # Reshape the images to a grid
-        image = images.view(y, x, h, w, c)
-        
+        image = torch.stack(resized_images).view(y, x, max_h, max_w, c)
+    
         # Swap the axes to get the correct orientation
-        image = image.transpose(1, 2).contiguous().view(1, y*h, x*w, c)
+        image = image.transpose(1, 2).contiguous().view(1, y*max_h, x*max_w, c)
     
         return (image,)
 
